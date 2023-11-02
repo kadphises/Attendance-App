0a56523d59842d1dcd358a51ef58cd09c5357be8
Egoist <selfish.peopleexist@gmail.com>
2023-11-02 22:45:52 +0530

    entry and exit fixed

--- src/UserView/RecordScreen.jsx
+++ src/UserView/RecordScreen.jsx
@@ -1,26 +1,56 @@
 import { Link } from "react-router-dom";
 import { Button } from "reactstrap";
-import { addCheckInTime } from "../record-operation";
+import { addCheckInTime, addCheckOutTime } from "../record-operation";
 import { useContext } from "react";
 import AttendanceContext from "../Provider";
+import { toast } from "react-toastify";
+import { useState } from "react";
 
 const RecordScreen = () => {
   const [state] = useContext(AttendanceContext);
+  const [loading, setLoading] = useState(false);
   console.log(state);
 
-  const enterCheckInTime = async () => {};
+  const enterCheckInTime = async () => {
+    try {
+      setLoading(true);
+      await addCheckInTime();
+      toast.success("Check-in time recorded", { toastId: "recchckin" });
+    } catch (e) {
+      toast.error("Unable to record CheckIn time", {
+        toastId: "recchckinfail",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+  const enterCheckOutTime = async () => {
+    try {
+      setLoading(true);
+      await addCheckOutTime();
+      toast.success("Check-out time recorded", { toastId: "recchckout" });
+    } catch (e) {
+      toast.error("Unable to record Check-out time", {
+        toastId: "recchckoutfail",
+      });
+    } finally {
+      setLoading(false);
+    }
+  };
+
   return (
     <>
       <h3>Record your time</h3>
       <div className="mt-4">
         <div>
-          <Button color="primary" className="me-2" onClick={enterCheckInTime}>
-            Record Check-in TIme
-          </Button>
           <Button
             color="primary"
-            className="mx-2"
-            onClick={async () => await addCheckInTime()}>
+            className="me-2"
+            onClick={enterCheckInTime}
+            disabled={loading}>
+            Record Check-in TIme
+          </Button>
+          <Button color="primary" className="mx-2" onClick={enterCheckOutTime}>
             Record Check-out TIme
           </Button>{" "}
         </div>
--- src/record-operation.js
+++ src/record-operation.js
@@ -1,5 +1,6 @@
 import { collection, doc, getDoc, setDoc, updateDoc } from "firebase/firestore";
 import { db, auth } from "./firebase";
+
 const month = [
   "jan",
   "feb",
@@ -17,55 +18,69 @@ const month = [
 const date = new Date();
 
 export const addCheckInTime = async () => {
-  try {
-    /**
-     * Check if doc exists or not if not exists create one and entry otherwise update it
-     */
-    const docRef = doc(db, "usersList", auth.currentUser.email);
-    const docSnap = await getDoc(docRef);
-    const entryData = {
-      month: month[date.getMonth()],
-      date: date.getDate() + 3,
-      checkInTime: Date.now(),
-      checkOutTime: null,
-    };
+  /**
+   * Check if doc exists or not if not exists create one and entry otherwise update it
+   */
+  const docRef = doc(db, "usersList", auth.currentUser.email);
+  const docSnap = await getDoc(docRef);
+  const entryData = {
+    month: month[date.getMonth()],
+    date: date.getDate(),
+    checkInTime: "8.40PM",
+    checkOutTime: null,
+  };
 
-    if (docSnap.exists()) {
-      /**Update the data or add entry */
-      console.log("Document exists abd updating it");
-      const currentQueue = docSnap.data()?.list;
-      currentQueue.push(entryData);
+  if (docSnap.exists()) {
+    /**Update the data or add entry */
+    console.log("Document exists abd updating it");
+    const currentQueue = docSnap.data()?.list;
+    currentQueue.push(entryData);
 
-      // If the queue exceeds 45 entries, remove the oldest entry
-      if (currentQueue.length > 45) {
-        currentQueue.shift(); // Remove the oldest entry
-      }
-      await updateDoc(docRef, {
-        list: currentQueue,
-      });
-
-      console.log("Document data updated with new data");
-    } else {
-      // docSnap.data() will be undefined in this case
-      /**
-      first time entry so create one doc */
-      console.log("No such document Creating One");
-      const usersListRef = collection(db, "usersList");
-
-      await setDoc(doc(usersListRef, auth.currentUser.email), {
-        userEmail: auth.currentUser.email,
-        list: [
-          {
-            month: month[date.getMonth()],
-            date: date.getDate(),
-            checkInTime: Date.now(),
-            checkOutTime: Date.now(),
-          },
-        ],
-      });
-      console.log("document created and entry added");
+    // If the queue exceeds 45 entries, remove the oldest entry
+    if (currentQueue.length > 45) {
+      currentQueue.shift(); // Remove the oldest entry
     }
-  } catch (e) {
-    console.log(e);
+    await updateDoc(docRef, {
+      list: currentQueue,
+    });
+
+    console.log("Document data updated with new data");
+  } else {
+    // docSnap.data() will be undefined in this case
+    /**
+      first time entry so create one doc */
+    console.log("No such document Creating One");
+    const usersListRef = collection(db, "usersList");
+
+    await setDoc(doc(usersListRef, auth.currentUser.email), {
+      list: [entryData],
+    });
+    console.log("document created and entry added");
+  }
+};
+
+export const addCheckOutTime = async () => {
+  /**
+   * Check if doc exists or not if not exists create one and entry otherwise update it
+   */
+  const docRef = doc(db, "usersList", auth.currentUser.email);
+  const docSnap = await getDoc(docRef);
+
+  if (docSnap.exists()) {
+    /**Update the data or add entry */
+    console.log("found entry and updating it");
+    const currentQueue = docSnap.data()?.list;
+    if (currentQueue) {
+      const lastEntry = currentQueue[currentQueue.length - 1];
+      const newEntry = { ...lastEntry, checkOutTime: "10:23PM" };
+      currentQueue.pop();
+      currentQueue.push(newEntry);
+    }
+
+    await updateDoc(docRef, {
+      list: currentQueue,
+    });
+
+    console.log("Document data updated with new data");
   }
 };
